31f2c79c668966185352ed93753ba983ce184e91
Rolly Maulana Awangga <awangga@gmail.com>
2025-01-10 16:18:13 +0700

    first blood

---
+++ croot.js
@@ -0,0 +1,36 @@
+import {onClick,renderHTML} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.0/element.js";
+import {getHash,onHashChange} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.0/url.js";
+
+
+onHashChange(pilihanMenu);
+
+//document.getElementById("content").innerHTML="<h1>ini adalah isi dari konten</h1>";
+onClick("halo",ubahHeader);
+
+function pilihanMenu(){
+    console.log(getHash());
+    const hashpath=getHash();
+    if (hashpath==="main"){
+        console.log("ini kedetek main")
+        renderHTML('content','content/main.html');
+    }
+    if (hashpath==="about"){
+        renderHTML('content','content/about.html');
+    }
+}
+
+function ubahHeader(){
+    document.getElementById("halo").innerHTML=getHash();
+}
+
+renderHTML('content','content/main.html');
+
+
+document.addEventListener("scroll", function () {
+    const header = document.getElementById("header");
+    if (window.scrollY > 50) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+});
